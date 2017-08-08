29dd85c807282a32fa81242d029067fd23488232
dntzhang <kmdjs@qq.com>
2017-08-08 10:38:53 +0800

    update plugin

--- plugins/omi-touch/index.js
+++ plugins/omi-touch/index.js
@@ -32,39 +32,39 @@
         }
     }
 
-    OmiTouch.init = function(){
-        Omi.extendPlugin('omi-touch',function(dom, instance){
-            if(dom.alloyTouch) return;
-            var target = instance.refs[ dom.getAttribute('motionRef')];
-            Transform(target);
-            var initialValue = dom.getAttribute('initialValue');
-            if(initialValue){
-                target[dom.getAttribute('property') || "translateY"] = parseInt(initialValue);
-            }
 
-            var alloyTouch = new AlloyTouch({
-                touch: dom,//反馈触摸的dom
-                vertical: dom.getAttribute('vertical') === 'false' ? false : true,//不必需，默认是true代表监听竖直方向touch
-                target: target, //运动的对象
-                property: dom.getAttribute('property') || "translateY",  //被运动的属性
-                min:  getNum('min', dom), //不必需,运动属性的最小值
-                max:  getNum('max', dom), //不必需,滚动属性的最大值
-                sensitivity: getNum('sensitivity', dom) ,//不必需,触摸区域的灵敏度，默认值为1，可以为负数
-                factor: getNum('factor', dom) ,//不必需,表示触摸位移与被运动属性映射关系，默认值是1
-                step: getNum('step', dom),//用于校正到step的整数倍
-                bindSelf: dom.getAttribute('bindSelf') === 'true' ? true : false,
-                change: getHandler('change', dom, instance),
-                touchStart: getHandler('touchStart', dom, instance),
-                touchMove: getHandler('touchMove', dom, instance),
-                touchEnd: getHandler('touchEnd', dom, instance),
-                tap: getHandler('tap', dom, instance),
-                pressMove: getHandler('pressMove', dom, instance),
-                animationEnd: getHandler('animationEnd', dom, instance)
-            })
+    Omi.extendPlugin('omi-touch',function(dom, instance){
+        if(dom.alloyTouch) return;
+        var target = instance.refs[ dom.getAttribute('motionRef')];
+        Transform(target);
+        var initialValue = dom.getAttribute('initialValue');
+        if(initialValue){
+            target[dom.getAttribute('property') || "translateY"] = parseInt(initialValue);
+        }
 
-            dom.alloyTouch = alloyTouch
-        });
-    }
+        var alloyTouch = new AlloyTouch({
+            touch: dom,//反馈触摸的dom
+            vertical: dom.getAttribute('vertical') === 'false' ? false : true,//不必需，默认是true代表监听竖直方向touch
+            target: target, //运动的对象
+            property: dom.getAttribute('property') || "translateY",  //被运动的属性
+            min:  getNum('min', dom), //不必需,运动属性的最小值
+            max:  getNum('max', dom), //不必需,滚动属性的最大值
+            sensitivity: getNum('sensitivity', dom) ,//不必需,触摸区域的灵敏度，默认值为1，可以为负数
+            factor: getNum('factor', dom) ,//不必需,表示触摸位移与被运动属性映射关系，默认值是1
+            step: getNum('step', dom),//用于校正到step的整数倍
+            bindSelf: dom.getAttribute('bindSelf') === 'true' ? true : false,
+            change: getHandler('change', dom, instance),
+            touchStart: getHandler('touchStart', dom, instance),
+            touchMove: getHandler('touchMove', dom, instance),
+            touchEnd: getHandler('touchEnd', dom, instance),
+            tap: getHandler('tap', dom, instance),
+            pressMove: getHandler('pressMove', dom, instance),
+            animationEnd: getHandler('animationEnd', dom, instance)
+        })
+
+        dom.alloyTouch = alloyTouch
+    });
+
 
     if (typeof exports == "object") {
         module.exports = OmiTouch;
