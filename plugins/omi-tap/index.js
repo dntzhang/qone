9d943e0655216d849f21a4e2b5d19392a8f7d46e
dntzhang <kmdjs@qq.com>
2017-12-20 10:37:50 +0800

    omi-tap 0.1.0

--- plugins/omi-tap/index.js
+++ plugins/omi-tap/index.js
@@ -17,33 +17,42 @@
         var x1,
             y1,
             scrollTop
-        dom.addEventListener('touchstart', function (evt) {
+
+        dom.removeEventListener('touchstart', dom._preTouchStart, false)
+        dom.removeEventListener('touchend', dom._preTouchEnd, false)
+        dom.addEventListener('touchstart', start, false)
+        dom.addEventListener('touchend', end, false)
+
+        dom._preTouchStart = start
+        dom._preTouchEnd = end
+
+        function start(evt) {
             x1 = evt.touches[0].pageX
             y1 = evt.touches[0].pageY
             scrollTop = document.body.scrollTop
-        }, false)
+        }
 
-        dom.addEventListener('touchend', function (evt) {
+        function end(evt) {
             if (Math.abs(evt.changedTouches[0].pageX - x1) < 30 && Math.abs(evt.changedTouches[0].pageY - y1) < 30 && Math.abs(document.body.scrollTop - scrollTop) < 30) {
                 getHandler('tap', dom, instance)(evt)
             }
-        }, false)
-    });
+        }
+    })
 
     function getHandler(name, dom, instance) {
-        var value = dom.getAttribute(name);
+        var value = dom.getAttribute(name)
         if (value === null) {
             if (dom[name]) {
                 return function (evt) {
-                    dom[name].bind(instance)(evt, dom);
+                    dom[name].bind(instance)(evt, dom)
                 }
             }
-            return noop;
+            return noop
         } else {
             return function (evt) {
-                instance[value].bind(instance)(evt, dom);
+                instance[value].bind(instance)(evt, dom)
             }
         }
-    };
+    }
 
 })();
