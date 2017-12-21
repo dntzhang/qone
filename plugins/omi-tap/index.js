b6cb763d00244b50d139f5469a0a3a62309bee8a
dntzhang <kmdjs@qq.com>
2017-12-21 10:29:44 +0800

    new tutorial

--- plugins/omi-tap/index.js
+++ plugins/omi-tap/index.js
@@ -12,47 +12,47 @@
     }
 
 
-Omi.extendPlugin('omi-tap', function (dom, instance) {
+    Omi.extendPlugin('omi-tap', function (dom, instance) {
 
-    var x1,
-        y1,
-        scrollTop
+        var x1,
+            y1,
+            scrollTop
 
-    dom.removeEventListener('touchstart', dom._preTouchStart, false)
-    dom.removeEventListener('touchend', dom._preTouchEnd, false)
-    dom.addEventListener('touchstart', start, false)
-    dom.addEventListener('touchend', end, false)
+        dom.removeEventListener('touchstart', dom._preTouchStart, false)
+        dom.removeEventListener('touchend', dom._preTouchEnd, false)
+        dom.addEventListener('touchstart', start, false)
+        dom.addEventListener('touchend', end, false)
 
-    dom._preTouchStart = start
-    dom._preTouchEnd = end
+        dom._preTouchStart = start
+        dom._preTouchEnd = end
 
-    function start(evt) {
-        x1 = evt.touches[0].pageX
-        y1 = evt.touches[0].pageY
-        scrollTop = document.body.scrollTop
-    }
-
-    function end(evt) {
-        if (Math.abs(evt.changedTouches[0].pageX - x1) < 30 && Math.abs(evt.changedTouches[0].pageY - y1) < 30 && Math.abs(document.body.scrollTop - scrollTop) < 30) {
-            getHandler('tap', dom, instance)(evt)
+        function start(evt) {
+            x1 = evt.touches[0].pageX
+            y1 = evt.touches[0].pageY
+            scrollTop = document.body.scrollTop
         }
-    }
-})
 
-function getHandler(name, dom, instance) {
-    var value = dom.getAttribute(name)
-    if (value === null) {
-        if (dom[name]) {
-            return function (evt) {
-                dom[name].bind(instance)(evt, dom)
+        function end(evt) {
+            if (Math.abs(evt.changedTouches[0].pageX - x1) < 30 && Math.abs(evt.changedTouches[0].pageY - y1) < 30 && Math.abs(document.body.scrollTop - scrollTop) < 30) {
+                getHandler('tap', dom, instance)(evt)
             }
         }
-        return noop
-    } else {
-        return function (evt) {
-            instance[value].bind(instance)(evt, dom)
+    })
+
+    function getHandler(name, dom, instance) {
+        var value = dom.getAttribute(name)
+        if (value === null) {
+            if (dom[name]) {
+                return function (evt) {
+                    dom[name].bind(instance)(evt, dom)
+                }
+            }
+            return noop
+        } else {
+            return function (evt) {
+                instance[value].bind(instance)(evt, dom)
+            }
         }
     }
-}
 
 })();
